026e2e53266573eb2f1e902454849cd56eacfa5d
Natalia Orsi <naty.m.orsi@gmail.com>
2021-10-26 11:06:50 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
+import NavigationBar from "./NavigationBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+     <h1>Trabalhos em Patchwork</h1>
+     <h2>por Ivanete de Mesquita Orsi Vieira</h2>
     </div>
   );
 }
