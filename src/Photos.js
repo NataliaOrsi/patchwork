78738f3d7ae302f35985a7830d0a7b6331084a34
Natalia Orsi <naty.m.orsi@gmail.com>
2021-11-02 11:40:42 +0100

    Worked on the frontpage photos

--- src/Photos.js
+++ src/Photos.js
@@ -4,50 +4,33 @@ import "./Photos.css";
 export default function Photos() {
     return (
       <section className="Photos">
-        <div className="container">
-          <div className="row img-fluid">
-            <div className="col-4">
+        
               <img
                 src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/019/864/original/028.jpg?1635322952"
                 alt="colcha em patchwork"
+                className="bed"
               />
-            </div>
-            <div className="col-4">
               <img
                 src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/019/867/original/20191218_105406.jpg?1635323808"
                 alt="bolsa pequena em patchwork"
-                className="middle"
+                className="small-purse"
               />
-            </div>
-            <div className="col-4">
               <img
                 src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/019/866/original/20171214_140303.jpg?1635323103"
                 alt="bolsa grande em patchwork"
+                className="big-purse"
               />
-            </div>
-          </div>
-          <div className="row img-fluid">
-            <div className="col-4">
               <img
                 src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/019/865/original/264.jpg?1635323041"
                 alt="almofadas em patchwork"
+                className="cushions"
               />
-            </div>
-            <div className="col-4">
-              <img
-                src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/019/869/original/20201217_125919.jpg?1635324019"
-                alt="americanos em patchwork"
-                className="middle"
-              />
-            </div>
-            <div className="col-4">
+
               <img
                 src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/019/868/original/20191224_111304.jpg?1635323979"
                 alt="toalha de natal em patchwork"
+                className="christmas-table-cover"
               />
-            </div>
-          </div>
-        </div>
       </section>
     );
 }
