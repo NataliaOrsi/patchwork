251f060472a9d724ab625fa734afa963c54995f9
Natalia Orsi <naty.m.orsi@gmail.com>
2021-11-17 14:16:44 +0100

    Added some styles to the about page

--- src/About.js
+++ src/About.js
@@ -6,18 +6,20 @@ export default function About() {
       <section className="header">
         <h2>Sobre</h2>
       </section>
-      <p>
-        Minha relação com o artesanato vem de criança, ao ver a minha mãe
-        costurar, bordar e fazer crochê, sempre me interessei e aprendi com ela
-        a gostar dessa arte. O artesanato é para mim uma oportunidade de
-        realizar um trabalho sem que haja intermediários, do início ao fim. O
-        patchwork aprendi em 2005, através de uma instrutora que aprendeu esta
-        arte na França, e logo me apaixonei. Além disso, qualquer tipo de
-        atividade artesanal nos envolve, nos conecta, e ocupa nossa mente,
-        deixando nossosproblemas existenciais mais leves. Atualmente, já
-        aposentada, ocupo o meu tempo alternando as atividades domésticas,
-        culturais, de leitura com o artesanato.
-      </p>
+      <div className="about-paragraph">
+        <p>
+          Minha relação com o artesanato vem de criança, ao ver a minha mãe
+          costurar, bordar e fazer crochê, sempre me interessei e aprendi com ela
+          a gostar dessa arte. O artesanato é para mim uma oportunidade de
+          realizar um trabalho sem que haja intermediários, do início ao fim. O
+          patchwork aprendi em 2005, através de uma instrutora que aprendeu esta
+          arte na França, e logo me apaixonei. Além disso, qualquer tipo de
+          atividade artesanal nos envolve, nos conecta, e ocupa nossa mente,
+          deixando nossosproblemas existenciais mais leves. Atualmente, já
+          aposentada, ocupo o meu tempo alternando as atividades domésticas,
+          culturais, de leitura com o artesanato.
+        </p>
+      </div>
     </div>
   );
 }
