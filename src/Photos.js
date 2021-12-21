974245c5e030a4ead7b8ec2ac850efd01c2e159e
Natalia Orsi <naty.m.orsi@gmail.com>
2021-12-21 11:09:06 +0100

    Added usestate to the front page photos

--- src/Photos.js
+++ src/Photos.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import bedBig from "./img/bed_big.jpg";
 import bedSmall from "./img/bed_small.jpg";
 import embroideredBig from "./img/embroideredPurse_big.jpg";
@@ -9,6 +9,25 @@ import purseSmall from "./img/purse_small.jpg";
 import "./Photos.css";
 
 export default function Photos() {
+
+    let [source, setSource] = useState(bedBig);
+    let [subtitle, setSubtitle]= useState("Colcha em Patchwork")
+
+    function firstPhotoFeatured() {
+      setSource(bedBig);
+      setSubtitle("Colcha em Patchwork");
+    }
+
+    function secondPhotoFeatured() {
+      setSource(embroideredBig);
+      setSubtitle("Bolsa Pequena Bordada");
+    }
+
+    function thirdPhotoFeatured() {
+      setSource(purseBig);
+      setSubtitle("Bolsa Grande em Patchwork");
+    }
+
     return (
       <section className="Photos">
         <div className="portfolio-introduction">
@@ -16,15 +35,24 @@ export default function Photos() {
           <p>Trabalhos feito a mão, com foco em bordado e patchwork.</p>
         </div>
         <div className="featured-portfolio">
-          <img
-            src={bedBig}
-            alt="colcha em patchwork"
-            className="gallery-highlight"
-          />
+          <img src={source} alt={subtitle} className="gallery-highlight" />
           <div className="work-preview">
-            <img src={bedSmall} alt="Toalha de Mesa" className="work-active" />
-            <img src={embroideredSmall} alt="Bolsa Pequena Bordada" />
-            <img src={purseSmall} alt="Bolsa Grande em Patchwork" />
+            <img
+              src={bedSmall}
+              alt="Colcha em patchwork"
+              className="work-active"
+              onClick={firstPhotoFeatured}
+            />
+            <img
+              src={embroideredSmall}
+              alt="Bolsa Pequena Bordada"
+              onClick={secondPhotoFeatured}
+            />
+            <img
+              src={purseSmall}
+              alt="Bolsa Grande em Patchwork"
+              onClick={thirdPhotoFeatured}
+            />
           </div>
         </div>
       </section>
